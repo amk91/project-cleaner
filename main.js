1fe16247aa95ec7d94f3a2620ab617ff73fdd9cb
Pisanelli Agostino <agostino.pisanelli@microtec.eu>
2021-01-08 23:39:48 +0100

    Added class ProjectArchiver to better handle start and stop archiving

--- main.js
+++ main.js
@@ -1,14 +1,181 @@
 const { app, BrowserWindow, dialog, Menu } = require('electron')
 const ipc = require('electron').ipcMain
 const fs = require('fs')
-const findit = require('findit')
-const zip = require('7zip-min')
+const archiver = require('archiver')
+const dir = require('node-dir')
 const xml = require('xml2js')
 const xmlbuilder = require('xmlbuilder2')
+const globToRegExp = require('glob-to-regexp')
 
 let configsList = []
+
 let homeWindow = null
 let configurationsWindow = null
+
+class ProjectArchiver {
+    constructor(homeWindow = null) {
+        this.homeWindow = homeWindow
+
+        this.init()
+    }
+
+    init(
+        archiveFolderPath = "",
+        projectFolderName = "",
+        configurationId = -1,
+        options = {
+            archiveFormat: 'zip',
+            archiveLevel: 9,
+        }
+    ) {
+        this.stopArchiveProject = false
+
+        this.configurationId = configurationId
+
+        this.archive = archiver(
+            options.archiveFormat,
+            { zlib: { level: options.archiveLevel } }
+        )
+
+        this.archiveFolderPath = archiveFolderPath;
+        this.projectFolderName = projectFolderName;
+
+        this.archiveFilePath = ""
+        if (this.archiveFolderPath.length > 0 && this.projectFolderName.length > 0) {
+            this.archiveFilePath =
+                archiveFolderPath + '\\' +
+                projectFolderName + '.' +
+                options.archiveFormat
+
+            this.projectFolderPath =
+                archiveFolderPath + '\\' +
+                projectFolderName
+
+            this.outputFile = fs.createWriteStream(this.archiveFilePath)
+        }
+    }
+
+    async archiveProject() {
+        if (configurationId < 0) {
+            return
+        }
+
+        var config = null
+        for (let i = 0; i < configsList.length; ++i) {
+            if (configsList[i].id === configurationId) {
+                config = configsList[i]
+                break
+            }
+        }
+
+        if (config === null) {
+            return
+        }
+
+        const getAllDirFiles = function (dirPath, arrayOfFiles) {
+            let files = fs.readdirSync(dirPath)
+            arrayOfFiles = arrayOfFiles || []
+            files.forEach((file) => {
+                if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+                    arrayOfFiles = getAllDirFiles(dirPath + '/' + file, arrayOfFiles)
+                } else {
+                    arrayOfFiles.push(file)
+                }
+            })
+
+            return arrayOfFiles
+        }
+
+        new Promise((resolve, reject) => {
+            var files = getAllDirFiles(this.projectFolderPath, files)
+            if (files && files.length > 0) {
+                resolve(files)
+            } else {
+                reject()
+            }
+        }).then((files) => {
+            if (this.homeWindow) {
+                this.homeWindow.webContents.send('archive-project-number-of-files', files.length)
+            }
+        }).catch((reason) => {
+            //TODO: handle rejected promise
+        })
+
+        this.archive.pipe(this.outputFile)
+
+        this.archive.on('progress', (progress) => {
+            if (this.homeWindow) {
+                this.homeWindow.webContents.send('archive-project-progress', progress)
+            }
+        })
+
+        this.archive.on('warning', (error) => {
+            //TODO: handle warning
+        })
+
+        this.archive.on('error', (data) => {
+            //TODO: handle error
+        })
+
+        new Promise((resolve, reject) => {
+            dir.readFiles(
+                this.projectFolderPath,
+                (err, content, filename, next) => {
+                    var name = filename.split(this.archiveFolderPath).pop()
+
+                    var archiveFile = true
+                    for (
+                        let ruleIndex = 0;
+                        ruleIndex < config.regexRules.length && archiveFile;
+                        ruleIndex++
+                    ) {
+                        archiveFile = !config.regexRules[ruleIndex].test(name)
+                    }
+
+                    if (archiveFile) {
+                        this.archive.file(filename, { name: name })
+                    }
+
+                    if (this.stopArchiveProject) {
+                        reject('stop')
+                    } else {
+                        next()
+                    }
+                },
+                (err, files) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+                }
+            )
+        }).then(() => {
+            return this.archive.finalize()
+        }).then(() => {
+            if (homeWindow) {
+                homeWindow.webContents.send('archive-project-finalize')
+            }
+        }).catch((reason) => {
+            //TODO: handle rejected promise
+        })
+    }
+
+    stopArchivingProcess() {
+        if (!this.stopArchiveProject) {
+            this.stopArchiveProject = true
+
+            this.archive.pause()
+
+            this.outputFile.end()
+            this.outputFile.close()
+
+            this.archive.unpipe()
+
+            fs.unlinkSync(this.archiveFilePath)
+        }
+    }
+}
 
 async function onShowConfigurations() {
     configurationsWindow = new BrowserWindow({
@@ -47,7 +214,10 @@ function loadConfigurationFromFile() {
                 var config = {
                     id: parseInt(xmlConfigData.id[0]),
                     name: xmlConfigData.name[0],
-                    rules: xmlConfigData.rule
+                    rules: xmlConfigData.rule,
+                    regexRules:
+                        xmlConfigData.rule ?
+                        xmlConfigData.rule.map(x => globToRegExp(x)) : []
                 }
                 configsList.push(config)
             }
@@ -107,6 +277,8 @@ function runApp() {
 
     loadConfigurationFromFile()
 
+    const projectArchiver = new ProjectArchiver(homeWindow)
+
     const menuTemplate = [
         {
             label: 'File',
@@ -139,21 +311,36 @@ function runApp() {
         return result.filePaths[0]
     })
 
-    let archiveFilename = null
-    let archiveFilepath = null
-    ipc.on('archive-info', (event, archiveFolderPath, projectFolderName) => {
-        archiveFilename = projectFolderName
-        archiveFilepath = archiveFolderPath + '\\' + archiveFilename
+    let projectFolderPath = null
+    let projectFolderName = null
+    let archiveFolderPath = null
+    ipc.on('archive-info', (
+        event,
+        _projectFolderPath,
+        _archiveFolderPath,
+        _projectFolderName
+        ) => {
+            projectFolderPath = _projectFolderPath
+            projectFolderName = _projectFolderName
+            archiveFolderPath = _archiveFolderPath
+        }
+    )
+
+    ipc.on('configuration-select', (event, value) => {
+        projectArchiver.configurationId = configurationId = value
+    })
+
+    ipc.on('start-archive-project', (event) => {
+        projectArchiver.init(archiveFolderPath, projectFolderName, configurationId)
+        projectArchiver.archiveProject()
+    })
+
+    ipc.on('stop-archive-project', (event) => {
+        projectArchiver.stopArchivingProcess()
     })
 
     ipc.handle('update-configuration-select', async (event) => {
         return configsList
-    })
-
-    let configurationId = null
-    ipc.on('configuration-select', (event, value) => {
-        configurationId = value
-        console.log(configurationId)
     })
 
     homeWindow.once('ready-to-show', () => {
--- mainRenderer.js
+++ mainRenderer.js
@@ -1,13 +1,20 @@
 const ipc = require('electron').ipcRenderer
 
 function updateProjectType() {
-    const projectTypeSelect = document.getElementById('configurationSelect')
+    const configurationSelect = document.getElementById('configurationSelect')
     ipc.invoke('update-configuration-select').then((result) => {
-        for (var i = 0; i < result.length; i++) {
-            var option = document.createElement('option')
-            option.id = option.value = result[i]['id']
-            option.text = result[i]['name']
-            projectTypeSelect.add(option)
+        if (result) {
+            for (var i = 0; i < result.length; i++) {
+                var option = document.createElement('option')
+                option.id = option.value = result[i]['id']
+                option.text = result[i]['name']
+                configurationSelect.add(option)
+            }
+
+            if (result.length > 0) {
+                configurationSelect.value = result[0]['id'].toString()
+                ipc.send('configuration-select', parseInt(configurationSelect.value))
+            }
         }
     })
 }
@@ -40,7 +47,7 @@ projectFolderOpenButton.addEventListener('click', () => {
                 projectFolderPath.length
             )
 
-            ipc.send('archive-info', archiveFolderPath, projectFolderName)
+            ipc.send('archive-info', projectFolderPath, archiveFolderPath, projectFolderName)
         }
     })
 })
@@ -55,5 +62,29 @@ archiveFolderOpenButton.addEventListener('click', () => {
 
 const configurationSelect = document.getElementById('configurationSelect')
 configurationSelect.addEventListener('change', (event) => {
-    ipc.send('configuration-select', configurationSelect.value)
+    ipc.send('configuration-select', parseInt(configurationSelect.value))
+})
+
+// ipc.on('archive-project-finalize', () => {
+//     alert('archive-project-finalize')
+// })
+
+ipc.on('archive-project-number-of-files', (event, numberOfFiles) => {
+    document.getElementById('numberOfTotalFiles').textContent = numberOfFiles
+    document.getElementById('processedFileProgress').setAttribute('max', numberOfFiles)
+})
+
+ipc.on('archive-project-progress', (event, progress) => {
+    document.getElementById('numberOfProcessedFiles').textContent = progress.entries.processed
+    document.getElementById('processedFileProgress').setAttribute('value', progress.entries.processed)
+})
+
+const startButton = document.getElementById('startButton')
+startButton.addEventListener('click', (event) => {
+    ipc.send('start-archive-project')
+})
+
+const stopButton = document.getElementById('stopButton')
+stopButton.addEventListener('click', (event) => {
+    ipc.send('stop-archive-project')
 })
